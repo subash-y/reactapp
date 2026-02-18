490ab15c786a492f0ad59432a4f6d76333453447
subash-y <subash_yuvaraj@Subash.localdomain>
2026-02-18 07:32:54 +0000

    components created for navbar,course,footer

---
+++ src/Course.jsx
@@ -0,0 +1,13 @@
+const course1 = "HTML";
+
+function Course(){
+    return (
+        <div>
+            <image src="" alt="HTML"></image>
+            <h3>{course1}</h3>
+            <p>This is HTML Course</p>
+        </div>
+    );
+}
+
+export default Course
---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer(){
+    return (
+        <div>
+            <p>
+              &copy;  Subash Y | All rights are resevered.
+            </p>
+
+        </div>
+
+    );
+}
+
+export default Footer
