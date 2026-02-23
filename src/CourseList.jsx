8603336bf36ade9c0f044726ba2fadd54fce18ac
subash-y <subash_yuvaraj@Subash.localdomain>
2026-02-23 17:16:28 +0000

    error handling and loading gif img

--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -4,44 +4,61 @@ import { useEffect, useState } from 'react';
 
 function CourseList() {
     //useSet hook
-    const [courses,setCourses] = useState(null);//coursedata
+    const [courses, setCourses] = useState(null);//coursedata
+    const [error, setError] = useState(null);
 
-    useEffect(()=>{
-        console.log("useEffect called");
-        fetch('http://localhost:3000/courses')
-        .then(response => {console.log(response)
-            return response.json();
-        }).then(data => setCourses(data))
-        
-    },[]); //We usually don't update state in useEffect because it run infinte,but here we did because it run only one time we have empty dependency array[] here. 
+    useEffect(() => {
+        setTimeout(() => {
 
-    function handleDelete(id){
+            fetch('http://localhost:3000/courses')
+                .then(response => {
+                    console.log(response)
+                    if (!response.ok) {
+                        throw Error("Couldn't retrieve data.");
+                    }
+                    return response.json();
+                }).then(data => setCourses(data))
+                .catch((error) => {
+                    console.log(error.message);
+                    setError(error.message);
+                })
+
+        },1000)
+
+    }, []); //We usually don't update state in useEffect because it run infinte,but here we did because it run only one time we have empty dependency array[] here. 
+
+    function handleDelete(id) {
         const newCourses = courses.filter((course) => course.id != id)
         setCourses(newCourses);
     }
-/* SORTING AND FILTER
-
-    courses.sort((x,y)=> x.price - y.price) //Ascending Order
-
-    const vfmcourses = courses.filter((course)=>course.price<200);
-*/
-
-    if(!courses){
-        return <></>
-    }
-    const coursesList = courses.map((course) => 
-        <Course
-        key={course.id} 
-        name={course.name} 
-        price={course.price}
-        image={course.image} 
-        rating={course.rating} 
-        ondelete = {()=>handleDelete(course.id)}/>
+    /* SORTING AND FILTER
     
+        courses.sort((x,y)=> x.price - y.price) //Ascending Order
+    
+        const vfmcourses = courses.filter((course)=>course.price<200);
+    */
+
+    if (!courses) {
+        return (
+            <>
+                {!error && <img src="data/assets/loading.gif"></img>}
+                {error && <p class="error">{error}</p>}
+            </>
+        )
+    }
+    const coursesList = courses.map((course) =>
+        <Course
+            key={course.id}
+            name={course.name}
+            price={course.price}
+            image={course.image}
+            rating={course.rating}
+            ondelete={() => handleDelete(course.id)} />
+
     ); //Course Component Data as list [holds 4 course componet]
-    return(
+    return (
         <>
-        {coursesList}
+            {coursesList}
         </>
     );
 
