d7fd319f4878a50e5fa2c5c55b13f9d1be837013
subash-y <subash_yuvaraj@Subash.localdomain>
2026-02-23 03:58:35 +0000

    Click event handling

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,17 +1,25 @@
 import NOIMG from './assets/noimg.jpg'
 
-function Course({show, name = "Course Name Not Found", image = NOIMG, price,rating=0}) {
-    
-        return (
+
+
+function Course({ show, name = "Course Name Not Found", image = NOIMG, price, rating = 0 }) {
+
+    function BuyCourse(discount,e) {
+        console.log(name,"purchased with",discount,"% discount");
+        console.log(e);
+    }
+
+    return (
         <div className="card">
             <img src={image} alt={name} />
             <h3>{name}</h3>
             <p>${price}</p>
             <p>Rating:{rating}</p>
+            <button onClick={(event)=> BuyCourse(20,event)}>Buy now</button>
         </div>
 
     );
-    
+
 }
 
 
