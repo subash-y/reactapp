18f5291e89eb95953bb20f702687d61fb03f6afa
subash-y <subash_yuvaraj@Subash.localdomain>
2026-02-23 05:42:14 +0000

    use state hook

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import NOIMG from './assets/noimg.jpg'
 
 
 
 function Course({ show, name = "Course Name Not Found", image = NOIMG, price, rating = 0 }) {
 
+    const [purchased,setPurchased] = useState(false);
     function BuyCourse(discount,e) {
         console.log(name,"purchased with",discount,"% discount");
-        console.log(e);
+        setPurchased(true);
+        console.log(purchased);
     }
 
     return (
@@ -16,6 +19,7 @@ function Course({ show, name = "Course Name Not Found", image = NOIMG, price, ra
             <p>${price}</p>
             <p>Rating:{rating}</p>
             <button onClick={(event)=> BuyCourse(20,event)}>Buy now</button>
+            <p>{purchased?"Already purchased":"Get it now"}</p>
         </div>
 
     );
